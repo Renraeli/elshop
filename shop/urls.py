61823505d0ba575ebd5c8e2fa567af69330f57b3
unknown <cabinetnoir@mail.ru>
2021-06-09 21:12:24 +0500

    DetailView va ListView ishlatib class viewlar qo'shildi.

--- shop/urls.py
+++ shop/urls.py
@@ -1,13 +1,19 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.views import View
+from django.views.generic import TemplateView
+from shop.views import MahsulotListView, MahsulotView, IndexView
 from . import views
 
 urlpatterns = [
+    path('', IndexView.as_view(), name='index'),
+    path('mahsulot/<int:pk>/', MahsulotView.as_view()),
+    path('mahsulotlar/', MahsulotListView.as_view()),
     path('test/', views.testoviy, name='testoviy'),
     path('category/<int:category_id>/', views.category, name='category'),
-    path('', views.index, name='index'),
+    # path('', views.index, name='index'),
+    path('temp/', TemplateView.as_view(template_name='shop/temp.html')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- shop/views.py
+++ shop/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.core.paginator import Paginator
 from django.shortcuts import render
 from .models import Mahsulot, Toifa
+from django.views.generic import ListView, DetailView
 
 
 def index(request):
@@ -9,6 +10,19 @@ def index(request):
     mahsulotlar = Mahsulot.objects.filter(toifasi=toifa)
     toifalar = Toifa.objects.all()
     return render(request, 'shop/index.html', {'toifalar': toifalar, 'mahsulotlar': mahsulotlar})
+
+
+class IndexView(ListView):
+    model = Toifa
+    template_name = 'shop/index.html'
+    context_object_name = 'toifalar'
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # Add in a QuerySet of all the books
+        context['mahsulotlar'] = Mahsulot.objects.all()
+        return context
 
 
 def testoviy(request):
@@ -25,3 +39,12 @@ def category(request, category_id):
     return render(request, 'shop/index.html', {'toifalar': toifalar, 'mahsulotlar': mahsulotlar})
 
 
+class MahsulotListView(ListView):
+    model = Mahsulot
+    template_name = 'shop/mahsulot_list.html'
+    context_object_name = 'tovar_list'
+
+
+class MahsulotView(DetailView):
+    model = Mahsulot
+    template_name = 'shop/mahsulot.html'
